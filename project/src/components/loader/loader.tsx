1a8b6ddd63bc5b7e95cb13e195d1832f0e73cea2
Koblizka <flood.nya@gmail.com>
2021-11-08 23:33:51 +0300

    Добавил нормальный лоадер

--- project/src/components/loader/loader.tsx
+++ project/src/components/loader/loader.tsx
@@ -1,8 +1,15 @@
+import LoaderSpinner from 'react-loader-spinner';
+
 function Loader(): JSX.Element {
   return (
-    <p>
-        Loading...
-    </p>
+    <div style={ {margin: '0 auto'} }>
+      <LoaderSpinner
+        type="ThreeDots"
+        color="#00BFFF"
+        height={80}
+        width={80}
+      />
+    </div>
   );
 }
 
